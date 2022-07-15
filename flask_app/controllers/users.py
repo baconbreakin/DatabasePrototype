caee178b678bc9caf00bc8d8ef67a5dcf79b48c7
Zach <zacharyjamesbacon@gmail.com>
2022-07-14 21:56:51 -0500

    Early login page.

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -7,3 +7,18 @@ bcrypt = Bcrypt(app)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not User.validate_user(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": pw_hash
+    }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect('/dashboard')
